cb8cba3a91fca6ce9520150450d13d3c2821c80a
Igor V. Frolenkov <igor@frolenkov.ru>
2022-05-07 00:38:06 +0700

    debug

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -34,7 +34,7 @@ AppAsset::register($this);
 
 <footer class="footer mt-auto py-3 text-muted">
     <div class="container">
-        <p class="float-left">'&copy; Metaseller ' <?= date('Y') ?></p>
+        <p class="float-left">&copy; Metaseller <?= date('Y') ?></p>
         <p class="float-right"><?= Yii::powered() ?></p>
     </div>
 </footer>
--- views/site/index.php
+++ views/site/index.php
@@ -10,8 +10,8 @@ use yii\web\View;
 $this->title = 'Metaseller';
 
 echo Html::beginTag('div', ['class' => 'site-index']);
-echo Html::beginTag('div', ['class' => 'jumbotron']);
-echo Html::tag('h1', Yii::t('views', 'Welcome to the Tinkoff Contest!'));
+echo Html::beginTag('div', ['class' => 'jumbotron', 'style' => 'text-align: center']);
+echo Html::tag('h1', 'PHP Demo Robot for Tinkoff Contest!');
 
 if ($version = @shell_exec('git describe --abbrev=0 --tags')) {
     echo Html::tag('p', $version, ['class' => 'lead']);
