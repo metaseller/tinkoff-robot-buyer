455aead06c875bcd3adccf8bbebe4ab29a73b586
Igor V. Frolenkov <igor@frolenkov.ru>
2022-05-10 19:47:33 +0700

    UPD env configs

--- environments/prod/config/tinkoff-invest.php
+++ environments/prod/config/tinkoff-invest.php
@@ -5,4 +5,5 @@ $credentials = require __DIR__ . '/credentials.php';
 return [
     'secret_key' => $credentials['tinkoff_invest']['secret_key'],
     'account_id' => $credentials['tinkoff_invest']['account_id'],
+    'app_name' => 'metaseller.tinkoff-robot-buyer',
 ];
