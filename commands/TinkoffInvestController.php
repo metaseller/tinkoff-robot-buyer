0b33453b82d2908cf3cfbeab89f0870d0d8b03d6
Igor V. Frolenkov <igor@frolenkov.ru>
2025-05-03 23:22:38 +0700

    remove log

--- commands/TinkoffInvestController.php
+++ commands/TinkoffInvestController.php
@@ -1903,13 +1903,6 @@ class TinkoffInvestController extends Controller
                 for ($trailing_sensitivity = 0.1; $trailing_sensitivity <= 0.65; $trailing_sensitivity += 0.01) {
                     $portfolio = $this->modelingTrailingBuy($day_data, $strategy_increment_value, $strategy_increment_period, $buy_limit, $trailing_sensitivity);
 
-                    echo $trailing_sensitivity . PHP_EOL;
-
-                    if ($trailing_sensitivity === 0.48) {
-                        var_dump($portfolio);
-                        echo PHP_EOL;
-                    }
-
                     $trailing_sensitivity = NumbersHelper::printFloat($trailing_sensitivity, 2, false);
 
                     if (!isset($modeling_portfolios[$strategy_increment_value][$strategy_increment_period][$buy_limit][$trailing_sensitivity])) {
@@ -2064,11 +2057,6 @@ class TinkoffInvestController extends Controller
                 ];
 
                 $portfolio = $this->modelingTrailingBuy($history_data, $strategy_increment_value, $strategy_increment_period, $buy_limit, $trailing_sensitivity);
-
-                echo $trailing_sensitivity . PHP_EOL;
-
-                var_dump($portfolio);
-                echo PHP_EOL;
 
                 list($avg_lot_price, $lots_count, $spend_money) = static::portfolioAnalyse($portfolio);
 
